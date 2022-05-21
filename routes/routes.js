fb9c915ef5e2de749cb601b872ddba4c96e4af21
Julian Salamanca <99279145+julians55@users.noreply.github.com>
2022-05-20 21:42:52 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
-const fav = require('../api/favs');
+const favs = require('../api/favs');
 const authLocal = require('../auth/local');
 const user = require('../api/user');
 
 function routes(app){
-    app.use('/api/favs', fav);
+    app.use('/api/favs', favs);
     app.use('/api/user', user);
     app.use('/auth/local', authLocal);
 }
